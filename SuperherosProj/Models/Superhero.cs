fdf2b3741cfe1379831fa1ca1fd806f421f72d8f
unknown <mstiff1983@gmail.com>
2020-06-17 10:24:44 -0400

    I created a model called Superheros which has the properties of ID, Name, AlterEgo, PrimaryAbility, Secondary Ability and CatchPhrase

---
+++ SuperherosProj/Models/Superhero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperherosProj.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int ID;
+
+        public string Name;
+
+
+    }
+}
