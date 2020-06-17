693011003cf694b06334247c178ffa7f8ef817dc
unknown <mstiff1983@gmail.com>
2020-06-17 11:22:21 -0400

    In my Superhero constructor, I created a global variable _context and set it to equal context in my databasr

--- SuperherosProj/Controllers/SuperHeroController.cs
+++ SuperherosProj/Controllers/SuperHeroController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SuperherosProj.Data;
 
 namespace SuperherosProj.Controllers
 {
     public class SuperHeroController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public SuperHeroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
     }
 }
